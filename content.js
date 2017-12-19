c2b357a2995af3c8b2d82fb01d209283581c14d7
Aberham Haile <aberhamhaile@Aberhams-MacBook-Pro.local>
2017-12-19 00:50:05 -0500

    sort

--- content.js
+++ content.js
@@ -1,36 +1,63 @@
+
+
 showHide();
 
 function showHide() {
-    
 
 
-for (var j = 0; j < contents.childNodes.length; j++) {
+
+    for (var j = 0; j < contents.childNodes.length; j++) {
 
 
-   
-     if (
-         (
-         (comment[j].children[0].children[1].children[1].children[0].children[1].children[0] == null ) ||
-         (comment[j].children[0].children[1].children[1].children[0].children[1].children[0] != null  && comment[j].children[0].children[1].children[1].children[0].children[1].children[0].href == undefined)
-        )
-        && contents.childNodes[j].style.display == 'none')
-    
-        
-        
-        {
-        contents.childNodes[j].style.display = "block"
-        console.log(j + comment[j].children[0].children[1].children[1].children[0].children[1].children[0] )
-        
+
+        //  if (
+        //      (
+        //      (comment[j].children[0].children[1].children[1].children[0].children[1].children[0] == null ) ||
+        //      (comment[j].children[0].children[1].children[1].children[0].children[1].children[0] != null  && comment[j].children[0].children[1].children[1].children[0].children[1].children[0].href == undefined)
+        //     )
+        //     && contents.childNodes[j].style.display == 'none')
+
+
+
+        //     {
+        //     contents.childNodes[j].style.display = "block"
+        //     console.log(j + comment[j].children[0].children[1].children[1].children[0].children[1].children[0] )
+
+        // }
+        // else
+        if ((comment[j].children[0].children[1].children[1].children[0].children[1].children[0] == null) || (comment[j].children[0].children[1].children[1].children[0].children[1].children[0]
+            != null && comment[j].children[0].children[1].children[1].children[0].children[1].children[0].href == undefined)) {
+            comment[j].children[0].children[1].children[1].children[0].children[1].children[0] == null
+            contents.removeChild(contents.childNodes[j])
+            //console.log(comment[j].children[0].children[1].children[1].children[0].innerText);
+        }
+        else {
+            console.log(j + comment[j].children[0].children[1].children[1].children[0].children[1].children[0])
+        }
+
+
     }
-    else
-     if ((comment[j].children[0].children[1].children[1].children[0].children[1].children[0] == null) || (comment[j].children[0].children[1].children[1].children[0].children[1].children[0] 
-        != null  && comment[j].children[0].children[1].children[1].children[0].children[1].children[0].href == undefined))
-    { contents.childNodes[j].style.display = 'none'
- //console.log(comment[j].children[0].children[1].children[1].children[0].innerText);
+
+
+    var items = document.getElementsByClassName('style-scope ytd-item-section-renderer')
+
+    items = Array.prototype.slice.call(items);
+
+    items = Array.prototype.slice.call(items.slice(4, items.length - 2));
+
+    items.sort(function (a, b) {
+        return parseInt(a.children[0].children[0].children[1].children[1].children[0].children[1].children[0].href.split("&t=")[1]) - parseInt(b.children[0].children[0].children[1].children[1].children[0].children[1].children[0].href.split("&t=")[1]);
+    });
+
+    for (var i = 0, len = items.length; i < len; i++) {
+        var parent = items[i].parentNode;
+        var detatchedItem = parent.removeChild(items[i]);
+        parent.appendChild(detatchedItem);
     }
-    else {
-        console.log(j + comment[j].children[0].children[1].children[1].children[0].children[1].children[0] )
-    }
-    
 }
-}
+
+
+
+
+
+
---
+++ setup.js
@@ -0,0 +1,8 @@
+
+    var c = document.getElementById("comments");
+       var r = document.getElementById("related");
+       document.getElementById("related").appendChild(c);
+       related.removeChild(related.children[0]);
+       related.removeChild(related.children[0]);
+       related.removeChild(related.children[0]);
+
