83a3cf5ff2cd029eedb87c874bfe59d67923e16f
Aberham Haile <aberhamhaile@Aberhams-MacBook-Pro.local>
2017-12-19 21:28:17 -0500

    sort function moved into own scope

--- content.js
+++ content.js
@@ -1,38 +1,30 @@
+if (related.children.length == 3){
+    var c = document.getElementById("comments");
+    var r = document.getElementById("related");
+    document.getElementById("related").appendChild(c);
+    related.removeChild(related.children[0]);
+    related.removeChild(related.children[0]);
+    related.removeChild(related.children[0]);
 
+}
 
 showHide();
 
 function showHide() {
-
-
-
+  
     for (var j = 0; j < contents.childNodes.length; j++) {
 
-
-
-        //  if (
-        //      (
-        //      (comment[j].children[0].children[1].children[1].children[0].children[1].children[0] == null ) ||
-        //      (comment[j].children[0].children[1].children[1].children[0].children[1].children[0] != null  && comment[j].children[0].children[1].children[1].children[0].children[1].children[0].href == undefined)
-        //     )
-        //     && contents.childNodes[j].style.display == 'none')
-
-
-
-        //     {
-        //     contents.childNodes[j].style.display = "block"
-        //     console.log(j + comment[j].children[0].children[1].children[1].children[0].children[1].children[0] )
-
-        // }
-        // else
         if ((comment[j].children[0].children[1].children[1].children[0].children[1].children[0] == null) || (comment[j].children[0].children[1].children[1].children[0].children[1].children[0]
             != null && comment[j].children[0].children[1].children[1].children[0].children[1].children[0].href == undefined)) {
             comment[j].children[0].children[1].children[1].children[0].children[1].children[0] == null
             contents.removeChild(contents.childNodes[j])
             //console.log(comment[j].children[0].children[1].children[1].children[0].innerText);
+            
         }
         else {
             console.log(j + comment[j].children[0].children[1].children[1].children[0].children[1].children[0])
+            //  i = comment.length
+            // console.log('i = ', i)
         }
 
 
@@ -45,17 +37,24 @@ function showHide() {
 
     items = Array.prototype.slice.call(items.slice(4, items.length - 2));
 
-    items.sort(function (a, b) {
-        return parseInt(a.children[0].children[0].children[1].children[1].children[0].children[1].children[0].href.split("&t=")[1]) - parseInt(b.children[0].children[0].children[1].children[1].children[0].children[1].children[0].href.split("&t=")[1]);
-    });
+    items.sort(sortComments)
 
     for (var i = 0, len = items.length; i < len; i++) {
         var parent = items[i].parentNode;
         var detatchedItem = parent.removeChild(items[i]);
         parent.appendChild(detatchedItem);
     }
+
+    items.sort(sortComments)
+
 }
 
+
+
+
+function sortComments (a,b) {
+    return parseInt(a.children[0].children[0].children[1].children[1].children[0].children[1].children[0].href.split("&t=")[1]) - parseInt(b.children[0].children[0].children[1].children[1].children[0].children[1].children[0].href.split("&t=")[1]);
+}
 
 
 
--- setup.js
+++
@@ -1,8 +0,0 @@
-
-    var c = document.getElementById("comments");
-       var r = document.getElementById("related");
-       document.getElementById("related").appendChild(c);
-       related.removeChild(related.children[0]);
-       related.removeChild(related.children[0]);
-       related.removeChild(related.children[0]);
-
